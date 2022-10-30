085bddd771f95fceebb03ca55d31c34882a55ce2
lokilokionly <loki.character.lie@gmail.com>
2022-10-30 19:01:19 +0600

    start2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
